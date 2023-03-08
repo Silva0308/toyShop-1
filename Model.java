7b242ec1e432fd5a48e51983ec9460c3a4dee0ff
nadezhda <nadezhda.bessonova@gmail.com>
2023-03-08 11:12:35 +0500

    Добавлен метод getToy

--- Model.java
+++ Model.java
@@ -64,6 +64,45 @@ public class Model implements ProgrammFunction{
 
     @Override
     public void getToy(){
+        int idUser = Integer.parseInt(JOptionPane.showInputDialog(null,
+        "Введите Id участника: ",
+        "Выдача игрушки",
+        JOptionPane.INFORMATION_MESSAGE));
+        String str;
+        try {
+            File path = new File("PRIZE-TOY.txt");
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            ArrayList<String> priorityArray = new ArrayList<>();
+            while ((str = br.readLine()) != null) {
+                priorityArray.add(str);
 
+            }
+            br.close();
+
+            for (int j = 0; j < priorityArray.size(); j++) {
+                String[] secondArray = new String[3];
+                secondArray = priorityArray.get(j).split(";");
+                if (idUser == Integer.parseInt(secondArray[2])) {
+                    JOptionPane.showMessageDialog(null,
+                            "Поздравляем ваш выйгрыш\n" + secondArray[0] + "," + secondArray[1],
+                            "Выдача игрушки",
+                            JOptionPane.INFORMATION_MESSAGE);
+
+                    priorityArray.remove(j);
+                }
+            }
+            BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+            for (int k = 0; k < priorityArray.size(); k++) {
+                bw.write(priorityArray.get(k));
+                bw.newLine();
+            }
+            bw.close();
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Error",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
